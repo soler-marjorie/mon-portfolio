600564a4ccd25f6c455150daeacbfbd8725fbdde
soler marjorie <soler.marjorie81@gmail.com>
2023-09-05 17:43:48 +0200

    creation header and footer

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    $mainMenu = [
+        [
+            "page" => "index.php", 
+            "title" => "Services et occasions", 
+            "meta_description" => "Expérience dans la mécanique depuis 15 ans, nous saurons mettre nos compétences en oeuvre pour régler vos problèmes!"
+        ],
+
+        [
+            "page" => "témoignages.php", 
+            "title" => "Les témoignages", 
+            "meta_description" => "Nos fidèles clients nous laisse leur meilleures témoignages. Besoin d'un avis sur notre garage? Regardez notre section témoignages."
+        ],
+
+        [
+            "page" => "contact.php", 
+            "title" => "Contactez-nous", 
+            "meta_description" => "Vous souhaitez contacter le garage V. Parrot? Une question sur nos services ? Sur nos Occasions? Ou simplement un renseignement? Nous vous répondons dans les plus brefs délais afin de répondre à vos interrogations."
+        ]
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+
+    <!-- Boostrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    
+    <!-- google font -->
+    
+    
+    <!-- feuille de style css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Document</title>
+</head>
+<body>
+    
+<!-- Boostrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+
+</body>
+</html>
